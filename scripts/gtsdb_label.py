24782c844a427d37d3aba219623184ada124bd5b
jonasto <jonasto@student.chalmers.se>
2016-02-03 16:46:32 +0100

    I princip färdig, dubletter inuti train_gt.txt och de andra två måste fixas

--- scripts/gtsdb_label.py
+++ scripts/gtsdb_label.py
@@ -2,6 +2,7 @@ import sys
 from os import getcwd
 import os.path
 from PIL import Image
+import shutil
 
 if len(sys.argv) > 1:
     img_db_dir = os.path.abspath(sys.argv[1])
@@ -12,7 +13,7 @@ else:
     print("usage: python %s img_db_dir" % sys.argv[0])
     sys.exit(1)
 
-sets=[('train', 720), ('val', 90),('test', 90)]
+sets=[('train', 720), ('val', 810),('test', 900)]
 
 classes = [("0" + str(i))[-2:] for i in range(43)]
 
@@ -23,10 +24,10 @@ where x,y,width,height is in percentages
 def convert(size, box):
     dw = 1./size[0]
     dh = 1./size[1]
-    x = (box[0] + box[1])/2.0
-    y = (box[2] + box[3])/2.0
-    w = box[1] - box[0]
-    h = box[3] - box[2]
+    x = (box[0] + box[2])/2.0
+    y = (box[1] + box[3])/2.0
+    w = box[2] - box[0]
+    h = box[3] - box[1]
     x = x*dw
     w = w*dw
     y = y*dh
@@ -39,21 +40,35 @@ def get_image_size(image_number):
 
 def convert_txt(gt_row):
     out_file_name=gt_row[0].split(".")[0]+".txt"
-    with open('%s/FullIJCNN2013/labels/%s'%(img_db_dir, out_file_name),'w') as f:
+    with open('%s/FullIJCNN2013/labels/%s'%(img_db_dir, out_file_name),'a') as f:
         b=gt_row[1:5]
         size=get_image_size(gt_row[0])
         bb=convert(size,b)
-        f.write(gt_row[5] + " " + " ".join([str(a) for a in bb]) + '\n')
+        new_description = "%s %s\n" %(str(gt_row[5]), " ".join([str(a) for a in bb]))
+        f.write(new_description)
+
+def get_image_set(pic_number):
+    for image_set, n in sets:
+        if int(pic_number) < n:
+	    return(image_set)
 
 wd = getcwd()
+labels_dir = '%s/FullIJCNN2013/labels' %img_db_dir
+if os.path.exists(labels_dir):
+    shutil.rmtree(labels_dir)
+os.mkdir(labels_dir)
+for sub_dir, _ in sets:
+    sub_txt_path='%s/FullIJCNN2013/%s/%s_gt.txt'%(img_db_dir, sub_dir, sub_dir)
+    if os.path.exists(sub_txt_path):
+        os.remove(sub_txt_path)
 
 with open('%s/FullIJCNN2013/gt.txt' % img_db_dir) as read_f:
-    for image_set, n in sets:
-        with open('%s/FullIJCNN2013/%s/%s_gt.txt' %(img_db_dir, image_set, image_set), 'w') as subset_gt:
-            for i in xrange(n):
-                line = read_f.next()
-                gt_row=line.split(";")
-                for i in range (1,4):
-                    gt_row[i]=int(gt_row[i])
-                convert_txt(gt_row)
-                subset_gt.write('%s/FullIJCNN2013/%s' % (img_db_dir, gt_row[0]))
+    for line in read_f:
+        line = line.strip("\n")
+        gt_row=line.split(";")
+        for i in range (1,5):
+            gt_row[i]=int(gt_row[i])
+        convert_txt(gt_row)
+        image_set=get_image_set(int(gt_row[0].split(".")[0]))
+        with open('%s/FullIJCNN2013/%s/%s_gt.txt' %(img_db_dir, image_set, image_set), 'a') as subset_gt:
+            subset_gt.write('%s/FullIJCNN2013/%s\n' % (img_db_dir, gt_row[0]))
